909048cd7685c590acd9fa5db0d20a015fd5f2a0
kianu <thomasulee@gmail.com>
2023-03-30 02:00:37 -0400

    Customizing portfolio for myself

--- src/components/summary-item/index.jsx
+++ src/components/summary-item/index.jsx
@@ -25,7 +25,7 @@ const SummaryItem = ({ name, description, link = false, internal = false, github
         {link ? linkContent : name}
       </h3>
       <p className={classes.description}>{description}</p>
-      {github && <p className={classes.description}><a href={github}>front-end github</a></p>}
+      {github && <p className={classes.description}><a href={github} target="_blank" className='hover:underline'>front-end github</a></p>}
     </div>
   );
 };
