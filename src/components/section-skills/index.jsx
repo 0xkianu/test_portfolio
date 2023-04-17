189bdab97784935546c47472c181d67f161d6fdf
kianu <thomasulee@gmail.com>
2023-04-16 21:17:00 -0400

    skills

--- src/components/section-skills/index.jsx
+++ src/components/section-skills/index.jsx
@@ -8,7 +8,7 @@ const classes = {
   listWrapper: 'block mb-6 md:flex',
   name: 'font-semibold text-gray-900 pb-1',
   description: 'text-md text-gray-600 font-light',
-  image: 'w-10 max-w-10',
+  image: 'w-10 max-w-10 transform transition-all duration-150 hover:scale-105',
   listItem: 'me-4'
 };
 
@@ -18,9 +18,9 @@ const SectionSkills = ({ skills }) => {
       <h3 className={classes.name}>Languages & Frameworks</h3>
       <p className={classes.description}>
         <ul className={classes.listWrapper} >
-          <li className={classes.listItem}><img className={classes.image} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" />JavaScript (ES6+)</li>
-          <li className={classes.listItem}><img className={classes.image} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg" />Node.js</li>
-          <li className={classes.listItem}><img className={classes.image} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg" />Express.js</li>
+          <li className={classes.listItem}><img className={classes.image} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" /></li>
+          <li className={classes.listItem}><img className={classes.image} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg" /></li>
+          <li className={classes.listItem}><img className={classes.image} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg" /></li>
         </ul>
       </p>
       <p className={classes.description}>
