f14b8eb015e383e36cc713c7f0de36279e545c9c
kianu <thomasulee@gmail.com>
2023-04-16 20:16:06 -0400

    skills

--- src/components/section-skills/index.jsx
+++ src/components/section-skills/index.jsx
@@ -7,6 +7,7 @@ const classes = {
   wrapper: 'mb-6',
   name: 'font-semibold text-gray-900 pb-1',
   description: 'text-md text-gray-600 font-light',
+  image: 'w-full max-w-150 hover:scale-105'
 };
 
 const SectionSkills = ({ skills }) => {
@@ -15,7 +16,7 @@ const SectionSkills = ({ skills }) => {
       <h3 className={classes.name}>Languages & Frameworks</h3>
       <p className={classes.description}>
         <ul>
-          <li><img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" />
+          <li><img className={classes.image} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" />
 JavaScript (ES6+)</li>
           <li>Node.js</li>
           <li>Express.js</li>
