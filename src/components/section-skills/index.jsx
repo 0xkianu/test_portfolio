2ebf22c6c2e1453771b7896a124d5c1df88132fe
kianu <thomasulee@gmail.com>
2023-04-16 20:24:32 -0400

    skills

--- src/components/section-skills/index.jsx
+++ src/components/section-skills/index.jsx
@@ -5,9 +5,10 @@ import SummaryItem from '../summary-item';
 
 const classes = {
   wrapper: 'mb-6',
+  listWrapper: 'block mb-6 md:flex',
   name: 'font-semibold text-gray-900 pb-1',
   description: 'text-md text-gray-600 font-light',
-  image: 'w-25 max-w-25 hover:scale-105'
+  image: 'w-20 max-w-20 hover:scale-105'
 };
 
 const SectionSkills = ({ skills }) => {
@@ -15,7 +16,7 @@ const SectionSkills = ({ skills }) => {
     <Section title="Skills" className={classes.wrapper}>
       <h3 className={classes.name}>Languages & Frameworks</h3>
       <p className={classes.description}>
-        <ul >
+        <ul className={classes.listWrapper} >
           <li><img className={classes.image} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" />
 JavaScript (ES6+)</li>
           <li>Node.js</li>
