c16309286532ada1a823a3565a7db3642543aecb
kianu <thomasulee@gmail.com>
2023-04-16 21:49:52 -0400

    skills

--- src/components/section-skills/index.jsx
+++ src/components/section-skills/index.jsx
@@ -7,7 +7,7 @@ const classes = {
   wrapper: 'mb-6',
   listWrapper: 'block mb-6 md:flex',
   name: 'font-semibold text-gray-900 pb-1',
-  description: 'text-md text-gray-400 font-light',
+  description: 'text-sm text-gray-600 font-light',
   image: 'w-10 max-w-10 transform transition-all duration-150 hover:scale-105',
   listItem: 'mr-6'
 };
@@ -25,12 +25,12 @@ const SectionSkills = ({ skills }) => {
           </li>
           <li className={classes.listItem}>
             <div className='flex flex-col items-center'>
-              <img className={classes.image} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg" />
+              <img className={classes.image} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg" />Node.js
             </div>
           </li>
           <li className={classes.listItem}>
             <div className='flex flex-col items-center'>
-              <img className={classes.image} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg" />
+              <img className={classes.image} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg" />Express.js
             </div>
           </li>
         </ul>
